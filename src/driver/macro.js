cff7d6da565cd4f7b16768d6683d31b2fd69a81e
iRuxu <rx6@qq.com>
2020-06-22 21:34:54 +0800

    test error

--- src/driver/macro.js
+++ src/driver/macro.js
@@ -1,5 +1,6 @@
 import jquery from "jquery";
 let search = location.search.slice(1).split("=")[1];
+search = decodeURIComponent(search)
 jquery.ajax({
     async: true,
     url: "https://pull.j3cx.com/api/macro/query",
@@ -7,9 +8,11 @@ jquery.ajax({
     data: { name: search },
     dataType: "json",
     success: function(data) {
-        location.href = data.about;
+        console.log(data)
+        // location.href = data.about;
     },
-    error: function() {
-        location.href = "https://www.jx3box.com/macro/";
+    error: function(err) {
+        // location.href = "https://www.jx3box.com/macro/";
+        console.log(err)
     },
 });
