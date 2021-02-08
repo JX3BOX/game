347fd7de9ce4b36c0c054e3e39be824254db37f4
iRuxu <rx6@qq.com>
2021-02-08 23:55:10 +0800

    fix

--- src/service/flower.js
+++ src/service/flower.js
@@ -1,8 +1,8 @@
-// import { __spider } from "@jx3box/jx3box-common/js/jx3box.json";
+import { __spider } from "@jx3box/jx3box-common/js/jx3box.json";
 import { $next } from "@jx3box/jx3box-common/js/axios";
 import axios from "axios";
 
-let __spider = 'http://localhost:3002/'
+// let __spider = 'http://localhost:3002/'
 function getFlower(params) {
     return axios.get(__spider + "flower", {
         params: params,
