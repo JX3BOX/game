a17b8c3ebd9b357e9869b9f055ef8b6cedf8719c
Emil Zhai <root@derzh.com>
2022-09-06 15:09:56 +0800

    feat: 云端宏根据版本切换缘起/重制版

--- src/driver/macro.js
+++ src/driver/macro.js
@@ -1,18 +1,22 @@
 import jquery from "jquery";
-const qs = require('qs');
-let search = qs.parse(location.search,{ ignoreQueryPrefix: true }).name
-search = decodeURIComponent(search)
+import { __Root, __OriginRoot } from "@jx3box/jx3box-common/data/jx3box.json";
+const qs = require("qs");
+
+const query = qs.parse(location.search, { ignoreQueryPrefix: true });
+const rootPath = query.L === "classic_yq" ? __OriginRoot : __Root;
+const name = decodeURIComponent(query.name);
+
 jquery.ajax({
     async: true,
     url: "https://pull.j3cx.com/api/macro/query",
     type: "GET",
-    data: { name: search },
+    data: { name: name },
     dataType: "json",
-    success: function(data) {
+    success: function (data) {
         // alert(search)
         location.href = data.about;
     },
-    error: function(err) {
-        location.href = "https://www.jx3box.com/macro/";
+    error: function (err) {
+        location.href = rootPath + "macro/";
     },
 });
